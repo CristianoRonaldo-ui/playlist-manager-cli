e3f96d181a76744042e0933bca82057486a384a4
Gia Bao Vu <25vll@queensu.ca>
2026-09-18 23:26:10 -0400

    Add Playlist class with composition (has-a Song list)

---
+++ src/playlist.py
@@ -0,0 +1,23 @@
+from src.song import Song 
+
+class Playlist:
+    def __init__ (self, name):
+        self.name = name 
+        self.songs = []
+
+    def add_song(self, song: Song):
+        self.songs.append(song)
+
+    def total_duration(self) -> int:
+        total = 0
+        for song in self.songs:
+            total += song.duration 
+        return total 
+    def find_by_artist(self, artist_name: str):
+        result = []
+        for song in self.songs:
+            if song.artist == artist_name:
+                result.append(song)
+        return result 
+        
+    
